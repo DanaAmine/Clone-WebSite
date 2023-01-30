aec04744b2b1418ace89f62c730b1de69d93b058
AmineBrd <aminebourada7@gmail.com>
2023-01-30 20:46:43 +0000

    first page

---
+++ amine/index.js
@@ -0,0 +1,28 @@
+let bar = document.querySelector(".bars");
+let ul = document.querySelector("ul");
+let nav = document.querySelector("nav");
+let logo = document.querySelector('#logo');
+let checked = true;
+(function responsivity(){
+if(window.innerWidth <= 900 ){
+    ul.style.display = "none";
+}else{
+    ul.style.display = "none";
+}
+})
+// 
+bar.addEventListener('click',()=>{
+    if(checked){
+        
+        ul.style.display = "flex";
+        checked = false;
+        nav.style.justifyContent = "space-evenly"
+    }else{
+        logo.style.display = 'block';
+        ul.style.display = "none";
+        checked = true;
+        nav.style.justifyContent = "space-between"
+        }
+        
+    }
+)
