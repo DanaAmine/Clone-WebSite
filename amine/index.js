2484bc790cbc49dcd38b6fdd5cbe8b02ed083f47
AmineBrd <69419525+AmineBrd@users.noreply.github.com>
2023-02-01 22:07:12 +0100

    Add files via upload

--- amine/index.js
+++ amine/index.js
@@ -1,29 +1,9 @@
-
 let bars = document.querySelector(".bars");
-let ul = document.querySelector("ul");
 let nav = document.querySelector("nav");
-let logo = document.querySelector('#logo');
-let checked = true;
-(function responsivity(){
-if(window.innerWidth <= 900 ){
-    ul.style.display = "none";
-}else{
-    ul.style.display = "none";
-}
+let ul = document.querySelector("ul");
+let li = document.querySelectorAll(".icons");
+bars.addEventListener("click",()=>{
+    nav.classList.toggle("nav-show")
+    ul.classList.toggle("ul-show")
+    li.classList.toggle("icons-hide")
 })
-// 
-bar.addEventListener('click',()=>{
-    if(checked){
-        
-        ul.style.display = "flex";
-        checked = false;
-        nav.style.justifyContent = "space-evenly"
-    }else{
-        logo.style.display = 'block';
-        ul.style.display = "none";
-        checked = true;
-        nav.style.justifyContent = "space-between"
-        }
-        
-    }
-)
